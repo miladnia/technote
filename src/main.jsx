3bf4122ea4200961d0c7c81786df9766497c8227
Milad Nia <miladniaa@gmail.com>
2025-03-05 03:02:10 +0330

    refactor: import Bootstrap and the css files in the React app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/bs-mod.css';
+import './assets/layout.css';
+import './assets/note-content.css';
 import './index.css';
 import App from './App.jsx';
 
