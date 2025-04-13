06d9d80b0e159f49c1adffe6948e8a836ec82239
Milad Nia <miladniaa@gmail.com>
2025-04-13 21:37:00 +0330

    build: add alias for directories

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "./",
   build: {
     manifest: true,
-    outDir: 'app/static/dist',
+    outDir: "app/static/dist",
     rollupOptions: {
       // overwrite default .html entry
-      input: 'src/web-client/main.jsx',
+      input: "src/web-client/main.jsx",
     },
     copyPublicDir: false,
   },
   server: {
-    origin: 'http://localhost:5173',
+    origin: "http://localhost:5173",
     port: 5173,
     strictPort: true,
   },
-})
+  resolve: {
+    alias: {
+      "@modules": path.resolve(__dirname, "./src/web-client/modules"),
+      "@ui": path.resolve(__dirname, "./src/web-client/ui"),
+      "@utils": path.resolve(__dirname, "./src/web-client/utils"),
+    },
+  },
+});
