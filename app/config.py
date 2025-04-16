37f34261e45e8c5759233d1bf136865c3b3bb8ce
Milad Nia <miladniaa@gmail.com>
2025-04-16 11:55:19 +0330

    move the example notes and the instance directory to the app directory

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+APP_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = Path(__file__).resolve().parent.parent
 
 CACHE_ENABLED = True
-CACHE_DIR = BASE_DIR / "instance/cache"
-DATABASE_FILE = BASE_DIR / "instance/notes.db"
-DATABASE_SCHEMA = BASE_DIR / "resources/schema.sql"
-PANDOC_TEMPLATE = BASE_DIR / "app/templates/pandoc.html"
-EXAMPLE_NOTES_DIR = BASE_DIR / "examples/example_tech_notes"
-VITE_MANIFEST_FILE = BASE_DIR / "app/static/dist/.vite/manifest.json"
+CACHE_DIR = APP_DIR / "instance/cache"
+DATABASE_FILE = APP_DIR / "instance/notes.db"
+DATABASE_SCHEMA = PROJECT_DIR / "resources/schema.sql"
+PANDOC_TEMPLATE = APP_DIR / "templates/pandoc.html"
+EXAMPLE_NOTES_DIR = APP_DIR / "examples/example_tech_notes"
+VITE_MANIFEST_FILE = APP_DIR / "static/dist/.vite/manifest.json"
 DEV_VITE_MAIN_FILE = "/src/web-client/main.jsx"
